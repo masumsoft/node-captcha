f4bfdd405af3af945fea09a826e5132e342f806f
Masum <masumsofts@yahoo.com>
2014-05-06 00:07:17 +0600

    fixing captcha size and image caching issues

--- captcha.js
+++ captcha.js
@@ -7,29 +7,29 @@ module.exports = function(params){
     params.background = params.background || 'rgb(255,200,150)';
 
 	return function(req, res, next){
-		if(req.path != params.url)
+		if(req.url != params.url)
 			return next();
 
-		var canvas = new Canvas(250, 150);
+		var canvas = new Canvas(220, 60);
 		var ctx = canvas.getContext('2d');
 		ctx.antialias = 'gray';
 		ctx.fillStyle = params.background;
-		ctx.fillRect(0, 0, 250, 150);
+		ctx.fillRect(0, 0, 220, 60);
 		ctx.fillStyle = params.color;
-		ctx.lineWidth = 8;
+		ctx.lineWidth = 3;
 		ctx.strokeStyle = params.color;
-		ctx.font = '80px sans';
-
+		ctx.font = '40px sans';
+		
 		for (var i = 0; i < 2; i++) {
-			ctx.moveTo(20, Math.random() * 150);
-			ctx.bezierCurveTo(80, Math.random() * 150, 160, Math.random() * 150, 230, Math.random() * 150);
+			ctx.moveTo(20, Math.random() * 60);
+			ctx.bezierCurveTo(70, Math.random() * 60, 140, Math.random() * 60, 210, Math.random() * 60);
 			ctx.stroke();
 		}
-
+		
 		var text = ('' + Math.random()).substr(3, 6);
 
-		for (i = 0; i < text.length; i++) {
-			ctx.setTransform(Math.random() * 0.5 + 1, Math.random() * 0.4, Math.random() * 0.4, Math.random() * 0.5 + 1, 30 * i + 20, 100);
+		for (var i = 0; i < text.length; i++) {
+			ctx.setTransform(Math.random() * 0.5 + 1, Math.random() * 0.4, Math.random() * 0.4, Math.random() * 0.5 + 1, 30 * i + 20, 45);
 			ctx.fillText(text.charAt(i), 0, 0);
 		}
 
@@ -41,6 +41,7 @@ module.exports = function(params){
 		canvas.toBuffer(function(err, buf) {
             if(req.session)
 			    req.session.captcha = text;
+            
 			res.end(buf);
 		});
 	};
